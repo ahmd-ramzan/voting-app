a248b800c37383cfe5a85f87a7fbc6075d9cca07
ahmd-ramzan <iamahmdrmzn@gmail.com>
2022-12-20 12:15:34 +0500

    livewire componenet to fetch ideas and pagination

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -15,16 +15,7 @@ class IdeaController extends Controller
      */
     public function index()
     {
-        return view('ideas.index', [
-            'ideas' => Idea::with(['user', 'category', 'status'])
-                ->addSelect(['voted_by_user' => Vote::select('id')
-                    ->where('user_id', auth()->id())
-                    ->whereColumn('idea_id', 'ideas.id')
-                ])
-                ->withCount('votes')
-                ->latest('id')
-                ->simplePaginate(Idea::PAGINATION_COUNT)
-        ]);
+        return view('ideas.index');
     }
 
     /**
