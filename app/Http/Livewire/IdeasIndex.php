7a839d36331399737973648c388f801c0acdfc67
ahmd-ramzan <iamahmdrmzn@gmail.com>
2022-12-23 09:14:34 +0500

    other filters

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -15,8 +15,9 @@ class IdeasIndex extends Component
 
     public $status = 'All';
     public $category;
+    public $filter;
 
-    protected $queryString = ['status', 'category'];
+    protected $queryString = ['status', 'category', 'filter'];
 
     public function mount()
     {
@@ -26,6 +27,20 @@ class IdeasIndex extends Component
     public function updatingCategory()
     {
         $this->resetPage();
+    }
+
+    public function updatingFilter()
+    {
+        $this->resetPage();
+    }
+
+    public function updatedFilter()
+    {
+        if ($this->filter === 'My Ideas') {
+            if (! auth()->check()) {
+                return redirect()->route('login');
+            }
+        }
     }
 
     protected $listeners = ['queryStringUpdatedStatus'];
@@ -48,6 +63,12 @@ class IdeasIndex extends Component
                 })
                 ->when($this->category && $this->category !== 'All Categories', function ($query) use ($categories) {
                     return $query->where('category_id', $categories->pluck('id', 'name')->get($this->category));
+                })
+                ->when($this->filter && $this->filter == 'Top Voted', function ($query) {
+                    return $query->orderByDesc('votes_count');
+                })
+                ->when($this->filter && $this->filter == 'My Ideas', function ($query) {
+                    return $query->where('user_id', auth()->id());
                 })
                 ->addSelect(['voted_by_user' => Vote::select('id')
                     ->where('user_id', auth()->id())
--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -9,10 +9,10 @@
             </select>
         </div>
         <div class="w-1/3">
-            <select name="other_filters" id="other_filters" class="w-full border-none rounded-xl px-4 py-2">
-                <option value="Category1">Category1</option>
-                <option value="Category1">Category1</option>
-                <option value="Category1">Category1</option>
+            <select wire:model="filter" name="other_filters" id="other_filters" class="w-full border-none rounded-xl px-4 py-2">
+                <option value="No Filter">No Filter</option>
+                <option value="Top Voted">Top Voted</option>
+                <option value="My Ideas">My Ideas</option>
             </select>
         </div>
         <div class="w-2/3 relative">
