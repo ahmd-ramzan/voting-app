c53ef79afdb398560c5c94cdb3e0ac264c13d007
ahmd-ramzan <iamahmdrmzn@gmail.com>
2022-12-27 07:56:17 +0500

    refactor modals

--- resources/views/ideas/show.blade.php
+++ resources/views/ideas/show.blade.php
@@ -9,23 +9,8 @@
     </div>
 
     <livewire:idea-show :idea="$idea" :votes-count="$votesCount" />
-    @can('update', $idea)
-        <livewire:edit-idea :idea="$idea" />
-    @endcan
 
-    @can('delete', $idea)
-        <livewire:delete-idea :idea="$idea" />
-    @endcan
-
-    @auth
-        <livewire:mark-idea-as-spam :idea="$idea" />
-    @endauth
-
-
-
-    @admin
-    <livewire:mark-idea-as-not-spam :idea="$idea" />
-    @endadmin
+    <x-modals-container :idea="$idea" />
 
     <div class="comments-container relative space-y-6 ml-22 pt-4 my-8 mt-1" style="margin-left: 70px">
         <div class="comment-container relative bg-white rounded-xl flex mt-4">
